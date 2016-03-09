370989a01696baedf52b3bda7dd76367e1320fed
Mark Vallis <1034528@my.brunel.ac.uk>
2016-03-09 14:07:16 +0000

    Removed directory output option and added new cases for functions

--- src/dronefyp1034528/Menu.java
+++ src/dronefyp1034528/Menu.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 public final class Menu {
     public Menu() throws IOException {
         
-        System.out.println("Marks first step to completing his FYP and degree!");
-        System.out.println("Would you like to see all the desktop items using dir?");
-        System.out.println("1 - Yes");
-        System.out.println("2 - Connect to drone and run WebFlight");
-        System.out.println("3 - Reconnect to Wifi@Brunel");
-        System.out.println("4 - Compare 2 images (console output)");
-        System.out.println("5 - Compare 2 images (new image output)");
-        System.out.println("6 - Output old, new and difference images");
-        System.out.println("7 - Exit" + "\n");
+        System.out.println("Mark's Drone Flight and Image Comparison Application");
+
+        System.out.println("1 - Connect to drone and run WebFlight");
+        System.out.println("2 - Reconnect to Wifi@Brunel");
+        System.out.println("3 - Compare 2 images (console output)");
+        System.out.println("4 - Compare 2 images (new image output)");
+        System.out.println("5 - Output old, new and difference images");
+        System.out.println("6 - Exit" + "\n");
         
         Scanner MenuChoice = new Scanner(System.in);
         int ChosenOption = MenuChoice.nextInt();
@@ -24,37 +23,40 @@ public final class Menu {
 
     public void MenuChoiceAction(int i) throws IOException {
                 
-        switch (i) {
+        switch (i) {          
             case 1:
-            {
-                ShowFileContents.showDir();
-                DroneFYP1034528.main(null);
-                break;
-            }          
-            case 2:
             {
                 DroneWifi.main();
                 StartWebflight.SWF();
                 DroneFYP1034528.main(null);
                 break;
             }
-            case 3:
+            case 2:
             {
                 WifiBrunel.main();
                 DroneFYP1034528.main(null);
                 break;
             }
-            case 4:
+            case 3:
             {        
                 compareImages.compare();                
                 break;
             }
+            case 4:
+            {
+                // "4 - Compare 2 images (new image output)"
+                break;
+            }                  
             case 5:
+            {
+                // "5 - Output old, new and difference images"
+                break;
+            } 
+            case 6:
             {
                 System.exit(0);
                 break;
-            }                        
-                
+            } 
             default:
             {
                 System.out.println("Incorrect entry, please choose an option between 1 and 4");
