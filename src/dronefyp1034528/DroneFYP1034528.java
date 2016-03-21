160abd8b2f76ab3c8f43d439f88927a342b4ce96
Mark Vallis <1034528@my.brunel.ac.uk>
2016-03-21 17:32:31 +0000

    Frontend GUI added for aesthetics over console app

--- src/dronefyp1034528/DroneFYP1034528.java
+++ src/dronefyp1034528/DroneFYP1034528.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 public class DroneFYP1034528 {
 
     public static void main(String[] args) throws IOException {
-        
-        Menu menu = new Menu();
-        
+
+        new FrontEndGUI().setVisible(true);     
     }    
 }
---
+++ src/dronefyp1034528/FrontEndGUI.java
@@ -0,0 +1,211 @@
+package dronefyp1034528;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FrontEndGUI extends javax.swing.JFrame {
+
+    public FrontEndGUI() {
+        super("Marks FYP application");
+        setLocationRelativeTo(null);
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        MainMenuPanel = new javax.swing.JPanel();
+        SWFButton = new javax.swing.JButton();
+        ReconWifIButton = new javax.swing.JButton();
+        CompConsoleButton = new javax.swing.JButton();
+        CompNewImageButton = new javax.swing.JButton();
+        OutputDiffImageButton = new javax.swing.JButton();
+        ExitButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        MainMenuPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Main Menu"));
+
+        SWFButton.setText("Connect to drone and run WebFlight");
+        SWFButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SWFButtonActionPerformed(evt);
+            }
+        });
+
+        ReconWifIButton.setText("Reconnect to Wifi@Brunel");
+        ReconWifIButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ReconWifIButtonActionPerformed(evt);
+            }
+        });
+
+        CompConsoleButton.setText("Compare 2 images (console output)");
+        CompConsoleButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CompConsoleButtonActionPerformed(evt);
+            }
+        });
+
+        CompNewImageButton.setText("Compare 2 images (new image output)");
+        CompNewImageButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CompNewImageButtonActionPerformed(evt);
+            }
+        });
+
+        OutputDiffImageButton.setText("Output JPanel overlaying diff and after image");
+        OutputDiffImageButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OutputDiffImageButtonActionPerformed(evt);
+            }
+        });
+
+        ExitButton.setText("Exit");
+        ExitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ExitButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout MainMenuPanelLayout = new javax.swing.GroupLayout(MainMenuPanel);
+        MainMenuPanel.setLayout(MainMenuPanelLayout);
+        MainMenuPanelLayout.setHorizontalGroup(
+            MainMenuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(MainMenuPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(MainMenuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(ExitButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(SWFButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(ReconWifIButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(CompConsoleButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(CompNewImageButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(OutputDiffImageButton, javax.swing.GroupLayout.DEFAULT_SIZE, 343, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        MainMenuPanelLayout.setVerticalGroup(
+            MainMenuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(MainMenuPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(SWFButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(ReconWifIButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(CompConsoleButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(CompNewImageButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(OutputDiffImageButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(ExitButton, javax.swing.GroupLayout.DEFAULT_SIZE, 52, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(MainMenuPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(MainMenuPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void ExitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExitButtonActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_ExitButtonActionPerformed
+
+    private void SWFButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SWFButtonActionPerformed
+        try {
+            ChangeWifi.main("ardrone2_062272");
+        } catch (IOException ex) {
+            Logger.getLogger(FrontEndGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        StartWebflight.SWF();
+    }//GEN-LAST:event_SWFButtonActionPerformed
+
+    private void ReconWifIButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ReconWifIButtonActionPerformed
+        try {
+            ChangeWifi.main("Wifi@Brunel");
+        } catch (IOException ex) {
+            Logger.getLogger(FrontEndGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_ReconWifIButtonActionPerformed
+
+    private void CompConsoleButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CompConsoleButtonActionPerformed
+        CompImagesSimple.compare();   
+    }//GEN-LAST:event_CompConsoleButtonActionPerformed
+
+    private void CompNewImageButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CompNewImageButtonActionPerformed
+        try {
+            CompImages.CI();
+        } catch (IOException ex) {
+            Logger.getLogger(FrontEndGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_CompNewImageButtonActionPerformed
+
+    private void OutputDiffImageButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OutputDiffImageButtonActionPerformed
+        OverlayDiff.OverlayOutput();
+        try {
+            // TODO upgrade JPanelOutput to just take any image URI for output
+            JPanelOutput.outputImg();
+        } catch (IOException ex) {
+            Logger.getLogger(FrontEndGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_OutputDiffImageButtonActionPerformed
+
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrontEndGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrontEndGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrontEndGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrontEndGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrontEndGUI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton CompConsoleButton;
+    private javax.swing.JButton CompNewImageButton;
+    private javax.swing.JButton ExitButton;
+    private javax.swing.JPanel MainMenuPanel;
+    private javax.swing.JButton OutputDiffImageButton;
+    private javax.swing.JButton ReconWifIButton;
+    private javax.swing.JButton SWFButton;
+    // End of variables declaration//GEN-END:variables
+}
